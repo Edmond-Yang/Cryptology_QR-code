8d474e1867b7309524903023b1e2d5a9a9e44559
Edmond Yang <yuenho092590@gmail.com>
2024-06-07 15:39:12 +0800

    init

---
+++ recover.py
@@ -0,0 +1,125 @@
+from sharing import PlaintextToHexSecretSharer
+import inquirer
+import animation
+import cv2
+from pyzbar import pyzbar
+
+
+def capture():
+    # 初始化攝影機
+    cap = cv2.VideoCapture(0)
+
+    # 初始化QR code檢測器
+    detector = cv2.QRCodeDetector()
+
+    while True:
+        # 讀取當前幀
+        _, img = cap.read()
+
+        # 檢測QR code
+        data, bbox, _ = detector.detectAndDecode(img)
+
+        # 如果有檢測到QR code
+        if data:
+            break
+
+    # 釋放資源
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return data
+
+
+def main():
+
+    # Enter shares
+
+    shares = []
+
+
+    while True:
+        questions = [
+            inquirer.List('share',
+                        message='Next share',
+                        choices=['Use Camera to Scan QR-code', 'Use QR-code Image',
+                                 'Use Text', 'Edit The Share', 'I don\'t have another one'],
+                    ),
+        ]
+        answer = inquirer.prompt(questions, raise_keyboard_interrupt=True)['share']
+
+        if answer == 'I don\'t have another one':
+            break
+        elif answer == 'Use Text':
+            txt = input('Enter your share: ')
+            if txt:
+                shares.append(txt)
+            else:
+                print('You don\'t input any text')
+        elif answer == 'Use QR-code Image':
+            txt = input('Enter Your Image Path: ')
+            if txt:
+                try:
+                    image = cv2.imread(txt)
+                    share = pyzbar.decode(image)[0].data.decode('utf-8')
+                    questions = [
+                        inquirer.List('confirm',
+                                      message=f'Confirm Your Share: {share}',
+                                      choices=['Yes', 'No'],
+                        ),
+                    ]
+                    answer = inquirer.prompt(questions, raise_keyboard_interrupt=True)['confirm']
+                    if answer == 'Yes':
+                        shares.append(share)
+
+                except:
+                    print('Error Occur. Please Try Again')
+            else:
+                print('You don\'t input any text')
+        elif answer == 'Use Camera to Scan QR-code':
+
+            wait = animation.Wait('spinner', 'Scanning QR-code.. It may take a while.. ')
+            wait.start()
+            try:
+                share = capture()
+                wait.stop()
+                questions = [
+                    inquirer.List('confirm',
+                                  message=f'Confirm Your Share: {share}',
+                                  choices=['Yes', 'No'],
+                    ),
+                ]
+                answer = inquirer.prompt(questions, raise_keyboard_interrupt=True)['confirm']
+                if answer == 'Yes':
+                    shares.append(share)
+            except:
+                wait.stop()
+                print('Error Occur. You should accept privacy for camera.')
+            pass
+        elif answer == 'Edit The Share':
+            questions = [
+                inquirer.List('confirm',
+                              message=f'Delete Your Share:',
+                              choices= shares + ['Delete All Above', 'Do Nothing'],
+                              ),
+            ]
+            answer = inquirer.prompt(questions, raise_keyboard_interrupt=True)['confirm']
+            if answer == 'Delete All Above':
+                shares = []
+            elif answer == 'Do Nothing':
+                pass
+            elif answer in shares:
+                shares.remove(answer)
+
+        else:
+            print('Error Occur.')
+
+    # Recover
+    wait = animation.Wait('spinner', 'Generating randomness.. It may take a while.. ')
+    wait.start()
+    message = PlaintextToHexSecretSharer.recover_secret(shares)
+    wait.stop()
+    print('Original message:\n'+message)
+
+
+if __name__ == '__main__':
+    main()
---
+++ share.py
@@ -0,0 +1,92 @@
+from sharing import PlaintextToHexSecretSharer
+import pyqrcode
+import inquirer
+import animation
+
+
+def main():
+	# Select number of shares
+	questions = [
+		inquirer.List('parties',
+			message='How many shares do you want?',
+			choices=['2', '3', '4', 'other'],
+		),
+	]
+	answer = inquirer.prompt(questions)
+	if answer['parties'] == 'other':
+		parties = int(input('Type a number: '))
+		while parties < 2:
+			parties = int(input('Type a number greater than 1: '))
+	else:
+		parties = int(answer['parties'])
+
+	# Select revealing threshold
+	if parties > 2:
+		min_threshold = 2
+		max_threshold = parties
+		thresholds = [x for x in range(min_threshold, max_threshold + 1)]
+		questions = [
+			inquirer.List('threshold',
+				message='How many shares should be enough for decryption? (Most secure: ' + str(
+				max_threshold) + ')',
+			    choices=thresholds,
+			),
+		]
+		answer = inquirer.prompt(questions)
+		threshold = int(answer['threshold'])
+	else:
+		threshold = parties
+
+	# Select type of shares output
+	questions = [
+		inquirer.List('format',
+			message='Select the format of output images',
+			choices=['png', 'svg', 'terminal'],
+		),
+	]
+	format = inquirer.prompt(questions, raise_keyboard_interrupt=True)['format']
+
+	# Select size of shares output
+	if format != 'terminal':
+		questions = [
+			inquirer.List('scale',
+				message='Size of output images',
+				choices=['Small', 'Medium', 'Large'],
+			),
+		]
+		answers = inquirer.prompt(questions, raise_keyboard_interrupt=True)
+		scale = 2
+		if answers['scale'] == 'Small':
+			scale = 2
+		elif answers['scale'] == 'Medium':
+			scale = 4
+		elif answers['scale'] == 'Large':
+			scale = 8
+
+	secret = input('Enter your message: ')
+	wait = animation.Wait('spinner', 'Generating randomness.. It may take a while.. ')
+	wait.start()
+	# Secret-share the message using Shamir's secret sharing scheme.
+	shares = PlaintextToHexSecretSharer.split_secret(secret, threshold, parties)
+	wait.stop()
+
+	print()
+	for share in shares:
+		print(share)
+	print()
+
+	for share in shares:  # Create png for each share
+		img = pyqrcode.create(share)
+		if format == 'png':
+			img.png(share[0] + '.png', scale=scale)
+			print(share[0] + '.png is Created.')
+		elif format == 'svg':
+			img.svg(share[0] + '.svg', scale=scale)
+			print(share[0] + '.svg is Created.')
+		elif format == 'terminal':
+			print(img.terminal())
+			print(share[0] + ' is Created.')
+
+
+if __name__ == '__main__':
+	main()
